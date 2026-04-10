972fcbbad0df12a3ce0ae9d7f75f1804f1d5e374
shaherakhtary01 <shahir.akh01@gmail.com>
2026-04-10 18:01:19 -0400

    Assignment 4 Part 1
    I did Raw text strings

--- Assignment4/part1/main.js
+++ Assignment4/part1/main.js
@@ -10,18 +10,13 @@ function randomValueFromArray(array) {
 }
 
 // Raw text strings
-
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
-
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
+const events = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and slithered away",
+];
 
 // Partial return random string function
 
